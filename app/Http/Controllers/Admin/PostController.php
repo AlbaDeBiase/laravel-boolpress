093c732f87c6abf9dfb114d653e2c93e962a71f0
Alba De biase <debiasealba@gmail.com>
2021-01-27 16:26:59 +0100

    definita rotta show controller show

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,13 +4,31 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use\App\Post;
 
 class PostController extends Controller
 {
-    public function(){
-        $data =[
-            'posts'->Post::all()
+    public function index() {
+        $data = [
+            'posts'=>Post::all()
         ];
-        return view('admin.posts.index';$data);
+        return view('admin.posts.index', $data);
     }
+
+    public function show($slug) {
+        $post= Post::where('slug', $slug)->first();
+        if(!$post) {
+            abort(404);
+        }
+
+        $data = ['post'=>$post];
+        return view('guest.posts.show', $data);
+
+    }
+
+
+
+
+
+
 }
